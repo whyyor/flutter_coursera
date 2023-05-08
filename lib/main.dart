d2deb916c8dac9c9fd71ebf44ce3227ce7b75ded
whyyor <whyyor@gmail.com>
2023-05-08 17:03:01 +0530

    add navbar with search sidebarbutton and avatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:designcode/constants.dart';
 import 'components/cards/recent_course_card.dart';
 import 'package:designcode/model/course.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +16,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         //scaffold is layout
-        body: SafeArea(
-          child: Container(
-            child: RecentCourseCard(
-              course: recentCourses[2],
+        body: Container(
+          color: kBackgroundColor,
+          child: SafeArea(
+            child: Column(
+              children: [
+                HomeScreenNavBar(),
+                RecentCourseCard(
+                  course: recentCourses[2],
+                ),
+              ],
             ),
           ),
         ),
@@ -27,3 +34,112 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class HomeScreenNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SidebarButton(),
+          SearchFieldWidget(),
+          const Icon(
+            Icons.notifications,
+            color: kPrimaryLabelColor,
+          ),
+          const SizedBox(
+            width: 16.0,
+          ),
+          const CircleAvatar(
+            radius: 18.0,
+            backgroundImage: AssetImage('asset/images/profile.jpg'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SearchFieldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Padding(
+      padding: const EdgeInsets.only(left: 12.0, right: 33.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14.0),
+          boxShadow: const [
+            BoxShadow(
+              color: kShadowColor,
+              offset: Offset(0, 12),
+              blurRadius: 16.0,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: TextField(
+            cursorColor: kPrimaryLabelColor,
+            decoration: InputDecoration(
+              icon: const Icon(
+                Icons.search,
+                color: kPrimaryLabelColor,
+                size: 20.0,
+              ),
+              border: InputBorder.none,
+              hintText: "Search for courses",
+              hintStyle: kSearchPlaceholderStyle,
+            ),
+            style: kSearchTextStyle,
+            onChanged: (newText) {
+              print(newText);
+            },
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class SidebarButton extends StatelessWidget {
+  const SidebarButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {
+        print("Sidebar button pressed");
+      },
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      // when we tap this button it will only tap to it's content
+      highlightColor: Colors.transparent,
+      splashColor: Colors.transparent,
+      constraints: const BoxConstraints(
+        maxWidth: 40.0,
+        maxHeight: 40.0,
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14.0),
+            boxShadow: const [
+              BoxShadow(
+                color: kShadowColor,
+                offset: Offset(0, 12.0),
+                blurRadius: 16.0,
+              )
+            ]),
+        child: Image.asset(
+          'asset/icons/icon-sidebar.png',
+          color: kPrimaryLabelColor,
+        ),
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 14.0),
+      ),
+    );
+  }
+}
