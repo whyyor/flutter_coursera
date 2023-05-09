77435f0463aafaa7c9e363766354a12ac4600ed8
whyyor <whyyor@gmail.com>
2023-05-09 13:10:38 +0530

    add all recent courses and bottom indicator with decorations

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,101 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: [
                 HomeScreenNavBar(),
-                RecentCourseCard(
-                  course: recentCourses[2],
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    //start only fills available area stretch fills entire area
+                    children: [
+                      Text(
+                        "Recents",
+                        style: kLargeTitleStyle,
+                      ),
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      Text(
+                        "23 courses, more coming",
+                        style: kSubtitleStyle,
+                      ),
+                    ],
+                  ),
                 ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                RecentCourseList(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+}
+
+class RecentCourseList extends StatefulWidget {
+  const RecentCourseList({super.key});
+
+  @override
+  State<RecentCourseList> createState() => _RecentCourseListState();
+}
+
+class _RecentCourseListState extends State<RecentCourseList> {
+  List<Container> indicators = [];
+  int currentPage = 0;
+
+  Widget updateIndicators() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: recentCourses.map(
+        (course) {
+          var index = recentCourses.indexOf(course);
+          return Container(
+            width: 7.0,
+            height: 7.0,
+            margin: const EdgeInsets.symmetric(horizontal: 6.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color:
+                  currentPage == index ? Color(0xFF0971FE) : Color(0xFFA6AEBD),
+            ),
+          );
+        },
+      ).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 320,
+          width: double.infinity,
+          // will occupy as much horizontal space available in parent
+          child: PageView.builder(
+            itemBuilder: (context, index) {
+              return Opacity(
+              opacity: currentPage == index ? 1.0 : 0.5,
+                child: RecentCourseCard(
+                  course: recentCourses[index],
+                ),
+              );
+            },
+            itemCount: recentCourses.length,
+            controller: PageController(initialPage: 0, viewportFraction: 0.63),
+            onPageChanged: (index) {
+              setState(() {
+                currentPage = index;
+              });
+            },
+          ),
+        ),
+        updateIndicators(),
+      ],
     );
   }
 }
