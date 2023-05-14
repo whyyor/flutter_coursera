ab2cd66bcffe1cabbfab4e384a82c6fcb6339ab1
whyyor <whyyor@gmail.com>
2023-05-15 00:40:26 +0530

    add content to course_screen and animation using recent course card

--- lib/components/cards/recent_course_card.dart
+++ lib/components/cards/recent_course_card.dart
@@ -44,25 +44,35 @@ class RecentCourseCard extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        course.courseSubtitle,
-                        style: kCardSubtitleStyle,
+                      Hero(
+                        tag: course.courseSubtitle,
+                        child: Text(
+                          course.courseSubtitle,
+                          style: kCardSubtitleStyle,
+                        ),
                       ),
                       const SizedBox(
                         height: 6.0,
                       ),
-                      Text(
-                        course.courseTitle,
-                        style: kCardTitleStyle,
+                      Hero(
+                        tag: course.courseTitle,
+                        child: Text(
+                          course.courseTitle,
+                          style: kCardTitleStyle,
+                        ),
                       ),
                     ],
                   ),
                 ),
                 Expanded(
+                  child: Hero(
                     child: Image.asset(
-                  'asset/illustrations/${course.illustration}',
-                  fit: BoxFit.cover,
-                )),
+                      'asset/illustrations/${course.illustration}',
+                      fit: BoxFit.cover,
+                    ),
+                    tag: course.illustration,
+                  ),
+                ),
                 // expanded makes sure the child take remaining space of parent widget
               ],
             ),
@@ -71,7 +81,12 @@ class RecentCourseCard extends StatelessWidget {
         Padding(
           padding: const EdgeInsets.only(right: 42.0),
           child: Container(
-            child: Image.asset('asset/logos/${course.logo}'),
+            child: Hero(
+              child: Image.asset('asset/logos/${course.logo}'),
+              tag: course.logo ?? "",
+              // we are passing nullable object in some cases
+              // so for that cases we use ""
+            ),
             width: 60.0,
             height: 60.0,
             decoration: BoxDecoration(
