95584f89574b279e4022a8e54f4c2e902c7cb4f1
whyyor <whyyor@gmail.com>
2023-05-16 13:22:05 +0530

    add certificates and completed courses in profile screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
 
+import 'package:designcode/components/cards/completed_courses_card.dart';
+import 'package:designcode/components/certificate_viewer.dart';
+import 'package:designcode/components/lists/completed_courses_list.dart';
 import 'package:designcode/constants.dart';
+import 'package:designcode/model/course.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -220,6 +224,76 @@ class ProfileScreen extends StatelessWidget {
                   ],
                 ),
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 32.0,
+                left: 20.0,
+                right: 20.0,
+                bottom: 12.0,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Certificates",
+                        style: kHeadlineLabelStyle,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "See all",
+                            style: kSearchPlaceholderStyle,
+                          ),
+                          const Icon(
+                            Icons.chevron_right,
+                            color: kSecondaryLabelColor,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const CertificateViewer(),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20.0,
+                right: 20.0,
+                bottom: 12.0,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Completed Courses",
+                        style: kHeadlineLabelStyle,
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "See all",
+                            style: kSearchPlaceholderStyle,
+                          ),
+                          const Icon(
+                            Icons.chevron_right,
+                            color: kSecondaryLabelColor,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const CompletedCoursesList(),
+            const SizedBox(
+              height: 28.0,
             ),
           ],
         ),
